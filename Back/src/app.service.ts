b7fb87c819267c05d662e2f61b2ddf45c066d591
Ken <kenlee2002@outlook.com>
2025-09-08 01:42:02 +0900

    fix:123123

--- Back/src/app.service.ts
+++ Back/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! 진짜 자동 배포 테스트!';
+    return 'Hello World! 찐막최종 자동배포 성공! 🚀';
   }
 }
