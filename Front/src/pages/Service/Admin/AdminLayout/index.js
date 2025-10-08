d38be44abeada22cc3e22635e249c9f904192131
yves <hyunbin0525@naver.com>
2025-10-08 16:33:02 +0900

    어드민처리

--- Front/src/pages/Service/Admin/AdminLayout/index.js
+++ Front/src/pages/Service/Admin/AdminLayout/index.js
@@ -1,6 +1,13 @@
 import {Outlet} from 'react-router-dom';
+import {useAuth} from '../../../../context/AuthContext.js';
 
 const AdminLayout = () => {
+  const {user} = useAuth();
+  
+  if (!user || user.role !== 'admin') {
+    return <div>접근 권한이 없습니다.</div>;
+  }
+  
   return(
     <div>
       <div>
